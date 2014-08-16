096e28cf7e05b9393f95cb47715954d96dd40b96
Bilal <bilal.arslan1993@hotmail.com>
2014-08-16 17:39:21 +0300

    Initial Commit

---
+++ ProjectEuler/ProjectEuler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEuler
+//
+//  Created by Bilal ARSLAN on 16/08/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
