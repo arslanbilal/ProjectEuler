f35b9bddc7198b6d4679ff96b61bde5c8d793311
Bilal <bilal.arslan1993@hotmail.com>
2014-08-17 21:37:57 +0300

    Solved Problem 6

--- ProjectEuler/ProjectEuler/Problems001to010.swift
+++ ProjectEuler/ProjectEuler/Problems001to010.swift
@@ -150,3 +150,18 @@ func problem005() -> Int
     }    
     return result
 }
+
+
+//Problem 6
+func problem006() -> Int
+{
+    var sumOfSquare: Int = 0
+    var squareOfSum: Int = 0
+    
+    for var i = 1; i <= 100; i++
+    {
+        sumOfSquare += i*i
+        squareOfSum += i
+    }
+    return ((squareOfSum * squareOfSum) - sumOfSquare)
+}
